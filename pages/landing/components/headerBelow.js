ef4a9dabe4b99d4a9776cdae508408f25ec87534
= <letiendat1999py@gmail.com>
2020-03-20 16:15:21 +0700

    (main-section):create teacher-student

---
+++ pages/landing/components/headerBelow.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import './belowHeader.scss';
+
+class headerBelow extends Component {
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className='col-6'>
+                        <div className="row">
+                            <div className="wrap-logo">
+                                <img src= './logo.png'/> 
+                            </div>
+                            <ul>
+                                <li>
+                                    <h2>Duy Tan</h2>
+                                </li>
+                                <li>
+                                    University
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='col-6'>
+                        <h1>
+                            FACE REGONITION
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default headerBelow;
--- pages/landing/home_sections/Main.js
+++ pages/landing/home_sections/Main.js
@@ -1,13 +1,23 @@
 import i18n from 'i18next';
 import Header from '../components/Header';
+import HeaderBelow from '../components/headerBelow';
+import InfoTecher from '../components/infoTecher';
+import SubjectCode from '../components/subjectCode';
+import StudentList from '../components/studentList';
 import "./Main.scss";
 
 
 class Main extends React.Component {
     render() {
         return (
-            <div className="main_section">
-                <Header />
+            <div className="main_section container"> 
+                <Header/>
+                <HeaderBelow/>
+                <div className="row">
+                    <div className="col-7"> <InfoTecher/></div>
+                    <div className="col-5"> <SubjectCode/></div>
+                </div>
+                <StudentList/>
             </div>
         );
     }
