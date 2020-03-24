467bbeb03ef189d2b8e17525b11c0b8a8640de83
HoangNam <phamhoangnam55@gmail.com>
2020-03-24 07:39:40 +0700

    Upsdate main_section

--- pages/landing/home.js
+++ pages/landing/home.js
@@ -1,9 +1,11 @@
 import "./main.scss"
+import Header from './components/Header';
 import Main from './home_sections/Main';
 class Home extends React.Component {
     render() {
         return (
             <span>
+                <Header />
                 <Main />
             </span >
         )
