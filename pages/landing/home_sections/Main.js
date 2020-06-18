cd229906edcc3186d10d536d1969350921f1a640
unknown <phamhoangnam55@gmail.com>
2020-06-18 17:18:25 +0700

    delete main

--- pages/landing/home_sections/Main.js
+++ pages/landing/home_sections/Main.js
@@ -7,7 +7,7 @@ class Main extends React.Component {
     render() {
         return (
             <div className="main_section">
-                <Header />
+               
             </div>
         );
     }
