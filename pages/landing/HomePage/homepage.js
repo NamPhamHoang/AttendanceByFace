6089ecfe0d7da28e0af3c6dc3ddceabee1438040
unknown <phamhoangnam55@gmail.com>
2020-06-20 12:44:48 +0700

    login_section_ver1

---
+++ pages/landing/HomePage/homepage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+const homepage = function Welcome(props) {
+    return (
+<div>
+
+</div>
+    )
+export default homepage
--- pages/landing/partner.js
+++ pages/landing/partner.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from '../landing/components/Header';
 import Login from './login_sections/login'
+import homepage from './HomePage/homepage'
 const partner = () => {
   return (
     <>
-
-      <Login />
+      <homepage/>
+      {/* <Login /> */}
     </>
   )
 }
