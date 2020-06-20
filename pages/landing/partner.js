c7ea5b74f055f8db3ef9ef7b4a64258d4cb0f875
NamPhamHoang <phamhoangnam55@gmail.com>
2020-06-20 15:48:43 +0700

    push login

--- pages/landing/partner.js
+++ pages/landing/partner.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Header from '../landing/components/Header';
 import Login from './login_sections/login'
-import homepage from './HomePage/homepage'
 const partner = () => {
   return (
     <>
-      <homepage/>
-      {/* <Login /> */}
+      <homepage />
+      <Login />
     </>
   )
 }
