234bb6e1c32f984fa6e478e0bac504734216ce51
NamPhamHoang <phamhoangnam55@gmail.com>
2020-06-21 21:36:11 +0700

    complete landing homesection

--- pages/landing/home.js
+++ pages/landing/home.js
@@ -3,9 +3,8 @@ import Main from './home_sections/Main';
 import Indispensable from './home_sections/Indispensable';
 import Partners from './home_sections/Partners';
 import NotConviced from './home_sections/NotConviced';
-import Pricing from './home_sections/Pricing';
+import Team from './home_sections/team';
 import Footer from './components/Footer';
-
 class Home extends React.Component {
     render() {
         return (
@@ -14,7 +13,7 @@ class Home extends React.Component {
                 <Indispensable />
                 <Partners />
                 <NotConviced />
-                <Pricing />
+                <Team />
                 <Footer />
             </span >
         )
